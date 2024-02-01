abd8d12c3af8bcb722efb37de0027da162835471
TuanVu <vuluu99pro@gmail.com>
2024-02-01 11:40:02 +0700

    fix

--- qwaco_sale/models/res_partner.py
+++ qwaco_sale/models/res_partner.py
@@ -16,11 +16,28 @@ class ResPartner(models.Model):
         tracking = True,
         help="This pricelist will be used, instead of the default one, for sales to the current partner")
 
-    sale_order_ids = fields.One2many('sale.order', 'partner_id', 'Sales Order',
-                                     domain=[
-                                         ('create_date', '<',
-                                          (datetime.now() + relativedelta(months=1)).strftime('%Y-%m-01 00:00:00')),
-                                         ('create_date', '>=',
-                                          (datetime.now() - relativedelta(months=0)).strftime('%Y-%m-01 00:00:00')),
-                                         ('is_paid','!=',True),('state', 'in', ['sale', 'done'])
-                                     ])
+    # sale_order_ids = fields.One2many('sale.order', 'partner_id', 'Sales Order',
+    #                                  domain=[
+    #                                      ('create_date', '<',
+    #                                       (datetime.now() + relativedelta(months=1)).strftime('%Y-%m-01 00:00:00')),
+    #                                      ('create_date', '>=',
+    #                                       (datetime.now() - relativedelta(months=0)).strftime('%Y-%m-01 00:00:00')),
+    #                                      ('is_paid','!=',True),('state', 'in', ['sale', 'done'])
+    #                                  ])
+
+    x_sale_order_ids = fields.Many2many('sale.order', compute="compute_order",string="Danh sách Báo giá")
+
+    def compute_order(self):
+        self.ensure_one()
+        if self.id:
+            domain = [('partner_id','=',self.id) ,
+                ('create_date', '<',
+                 (datetime.now() + relativedelta(months=1)).strftime('%Y-%m-01 00:00:00')),
+                ('create_date', '>=',
+                 (datetime.now() - relativedelta(months=0)).strftime('%Y-%m-01 00:00:00')),
+                ('is_paid', '!=', True), ('state', 'in', ['sale', 'done'])
+            ]
+            order_ids = self.env['sale.order'].search(domain)
+            self.x_sale_order_ids = [(6, 0, order_ids.ids)] or None
+        else:
+            self.x_sale_order_ids = None
